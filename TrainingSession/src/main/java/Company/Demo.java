32ab7f68a3f78574f64d1d5b73abedbe18c3d374
mansibansod0907-glitch <mansibansod0907@gmail.com>
2025-12-18 06:02:24 +0530

    ...

--- TrainingSession/src/main/java/Company/Demo.java
+++ TrainingSession/src/main/java/Company/Demo.java
@@ -4,8 +4,18 @@ package Company;
 public class Demo {
 
 	public static void main(String[] args) {
-		byte a = 10;
-		System.out.println(a);
+		int a = 45;
+		System.out.println(a);   //decimal
+		
+		int b = 045;
+		System.out.println(b); //octal
+		
+		int c= 0x45;
+		System.out.println(c); //hexadecimal
+		
+		int d = 0b1011;
+		System.out.println(d);  //binary
+	
 	}
 
 }
