709bec9df38667931778a78fd8da1529db90f93e
mansibansod0907-glitch <mansibansod0907@gmail.com>
2025-12-26 15:22:26 +0530

    Constructor

--- TrainingSession/src/main/java/com/encapsulation/EmployeeApp2.java
+++ TrainingSession/src/main/java/com/encapsulation/EmployeeApp2.java
@@ -3,15 +3,12 @@ package com.encapsulation;
 public class EmployeeApp2 {
 
 	public static void main(String[] args) {
-		Employee1 e1 = new Employee1();
-		System.out.println(e1.getemp_id());
-		System.out.println(e1.getname());
-		System.out.println(e1.getsalary());
 		
-//		Employee1 e2 = new Employee1(101,"Mansi",20);
-//		System.out.println(e2.getemp_id());
-//		System.out.println(e2.getname());
-//		System.out.println(e2.getsalary());
+		
+		Employee1 e2 = new Employee1(101,"Mansi",20);
+		System.out.println(e2.getemp_id());
+		System.out.println(e2.getname());
+		System.out.println(e2.getsalary());
 	
 
 	}
